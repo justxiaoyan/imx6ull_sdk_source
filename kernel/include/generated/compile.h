63a0235d68569aad6b6858b827aa7c32c4af5f0a
justxiaoyan <hushanyan1136@163.com>
2026-06-29 16:23:58 +0800

    [ADD][obc]增加imx6ull内核代码源码

---
+++ kernel/include/generated/compile.h
@@ -0,0 +1,4 @@
+#define UTS_MACHINE		"arm"
+#define LINUX_COMPILE_BY	"ayan-server"
+#define LINUX_COMPILE_HOST	"ayan-server"
+#define LINUX_COMPILER		"arm-buildroot-linux-gnueabihf-gcc.br_real (Buildroot 2024.08-git) 13.3.0, GNU ld (GNU Binutils) 2.41"
